d34e25ca01eeded19ef0b2fbfda261bb1da76958
Vikas Bhalla <77003706+vikasbhalla05@users.noreply.github.com>
2021-07-24 20:03:29 +0530

    Sorting an array using Bubble sorting
    Idea: if arr[i] > arr[i+1] swap them. To place the element in their respective
    position, we have to do the following operation N-1 times.
    Time Complexity: O(N​2​)

---
+++ BubbleSortInArray.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    
+    int count=0;
+    while(count<n-1){
+        for(int i=0;i<n-count-1;i++){
+            if(arr[i+1]<arr[i]){
+                int temp=arr[i+1];
+                arr[i+1]=arr[i];
+                arr[i]=temp;
+            }
+        }
+        count++;
+    }
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    
+    return 0;
+}
