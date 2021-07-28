70699daea09d8ac1010eb5f5d5e83d129a934980
Vikas Bhalla <77003706+vikasbhalla05@users.noreply.github.com>
2021-07-28 11:56:20 +0530

    Calculating the maximum subarray sum
    Approach 1
    self
    Calculating the sum of each subarray and maximising the sum

---
+++ MaxSubarraySumApproach1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int maxSum=INT_MIN;
+    for(int i=0;i<n;i++){
+        int sum=0;
+        for(int j=i;j<n;j++){
+            sum+=arr[j];
+            maxSum=max(sum,maxSum);
+        }
+    }
+    cout<<maxSum<<endl;;
+    return 0;
+}
