273115fb139352ccaf989e513d45e1f2ea142227
Vikas Bhalla <77003706+vikasbhalla05@users.noreply.github.com>
2021-08-01 19:09:33 +0530

    Make an string Upper and lower Case using transform!

---
+++ UpperLowerCaseAnString.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    string s="VikasBhalla";
+    
+    transform(s.begin(),s.end(),s.begin(),::toupper);
+    cout<<s<<endl;
+    
+    transform(s.begin(),s.end(),s.begin(),::tolower);
+    cout<<s<<endl;
+
+    return 0;
+}
